f5c3ccae19dc6522f7d70486d1ea463cc6870223
MunyinSam <kelvinsam233@gmail.com>
2023-11-19 07:31:53 +0700

    Save

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from settings import *
+from sys import exit
+#components
+
+from game import Game
+from score import Score
+
 
 class Main:
     def __init__(self):
@@ -6,6 +12,11 @@ class Main:
         # general things to do
         pygame.init()
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT), pygame.FULLSCREEN)
+        self.clock = pygame.time.Clock()
+        pygame.display.set_caption('Tetris')
+
+        self.game = Game()
+        self.score = Score()
 
     def run(self):
         while True:
@@ -14,8 +25,13 @@ class Main:
                     pygame.quit()
                     exit()
             
-            self.display_surface.fill('gray')
+            self.display_surface.fill(GRAY)
+
+            self.game.run()
+            self.score.run()
+
             pygame.display.update()
+            self.clock.tick() #fps
 
 
 if __name__ == '__main__': # we will only run this script
