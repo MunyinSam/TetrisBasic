bb55eab0c5bbda2c468cf4cbd8bd1a9a97d40ef5
MunyinSam <kelvinsam233@gmail.com>
2023-11-19 07:02:25 +0700

    General Things

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("Test")
+from settings import *
+
+class Main:
+    def __init__(self):
+        
+        # general things to do
+        pygame.init()
+        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT), pygame.FULLSCREEN)
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+            
+            pygame.display.update()
+
+
+if __name__ == '__main__': # we will only run this script
+    main = Main()
+    main.run()
